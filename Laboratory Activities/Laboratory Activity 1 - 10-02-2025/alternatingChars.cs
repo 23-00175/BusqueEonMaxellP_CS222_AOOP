a08776bbd703a9d48b88152fd13e424c0fcb2ef5
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2025-02-11 16:55:32 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 1 - 10-02-2025/alternatingChars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlternatingChars {
+    public class AlternatingChars {
+        static void Main() {
+            char firstChar, secondChar;
+            int size;
+
+            Console.Write("First character: ");
+            firstChar = Convert.ToChar(Console.ReadLine());
+            Console.Write("Second character: ");
+            secondChar = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter size: ");
+            size = Convert.ToInt32(Console.ReadLine());       
+
+            for (int i = 0; i < size; i++) {
+
+                for (int j = 0; j < i; j++) {
+                    Console.Write('-');
+                }
+
+                if (i % 2 == 0) 
+                    Console.WriteLine(firstChar);
+                else
+                    Console.WriteLine(secondChar);
+            }
+        }
+    }
+}
---
+++ Laboratory Activities/Laboratory Activity 1 - 10-02-2025/squarePattern.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SquarePattern {
+    public class SquarePattern {
+        static void Main() {
+            int rowsColumns, skipRow;
+
+            Console.Write("Enter n: ");
+            rowsColumns = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter y: ");
+            skipRow = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rowsColumns; i++) {
+                if (i == skipRow) {
+                    Console.WriteLine("");
+                    continue;
+                }
+
+                for (int j = 1; j <= rowsColumns; j++) {  // Fixed variable name and increment
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
