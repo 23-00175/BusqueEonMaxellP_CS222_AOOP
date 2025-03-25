86617c5678e6c2ff0894a7c0344ce7b0882f7dae
Eon Maxell Busque <165742990+23-00175@users.noreply.github.com>
2025-03-25 12:38:31 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 2 - 17-02-2025/CurrencyConverter.cs
@@ -0,0 +1,25 @@
+﻿/* Write a program that accepts an amount in one currency (e.g., USD) and
+converts it to another currency (e.g., EUR), based on a fixed exchange rate
+provided by the user.
+*/
+
+using System;
+
+namespace CurrencyConverter {
+    public class CurrencyConverter {
+        public static void Main() {
+
+            double usdAmount, exchangeRate, eurAmount;
+
+            Console.Write("Enter amount in USD: ");
+            usdAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter exchange rate from USD to EUR: ");
+            exchangeRate = Convert.ToDouble(Console.ReadLine());
+            
+            eurAmount = usdAmount * exchangeRate;
+
+            Console.WriteLine($"Amount in EUR: {eurAmount:F2}");
+        }
+    }
+}
---
+++ Laboratory Activities/Laboratory Activity 2 - 17-02-2025/InterestCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace interestCalculator {
+    public class InterestCalculator {
+        public static void Main() {
+
+            double principalAmount, interestRate, simpleInterest;
+            int years;
+
+            Console.Write("Enter principal amount: ");
+            principalAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter rate of interest: ");
+            interestRate = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter time in years: ");
+            years = Convert.ToInt32(Console.ReadLine());
+
+            simpleInterest = (principalAmount * interestRate * years) / 100;
+
+            Console.WriteLine("Simple Interest: " + simpleInterest);
+        }
+    }
+}
